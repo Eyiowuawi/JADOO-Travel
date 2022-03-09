d502162e5e15311c26eaf94c480aeff95cee1803
Adeosun Toluwalase <temilolaadeosun15@gmail.com>
2022-03-09 12:29:40 +0100

    carousel2

--- index.js
+++ index.js
@@ -6,75 +6,75 @@ toggleButton.addEventListener('click', () => {
 })
 
 //the Code that controls the carousel under the testimonial section
-const track = document.querySelector('testimonial-flexright');
-const slides = Array.from(track.children);
-console.log(slides)
+// const track = document.querySelector('testimonial-flexright');
+// const slides = Array.from(track.children);
+// console.log(slides)
 
-const slides = document.querySelectorAll('.slide');
-const next = document.querySelector('.next');
-const prev = document.querySelector('.prev');
-const slidedots = document.querySelector('.dots')
-const auto = true; // Auto scroll
-const intervalTime = 4000;
-let slideInterval;
+// const slides = document.querySelectorAll('.slide');
+// const next = document.querySelector('.next');
+// const prev = document.querySelector('.prev');
+// const slidedots = document.querySelector('.dots')
+// const auto = true; // Auto scroll
+// const intervalTime = 4000;
+// let slideInterval;
 
-const nextSlide = () => {
-    // Get current class
-    const current = document.querySelector('.current');
-    // Remove current class
-    current.classList.remove('current');
-    // Check for next slide
-    if (current.nextElementSibling) {
-        // Add current to next sibling
-        current.nextElementSibling.classList.add('current');
-    } else {
-        // Add current to start
-        slides[0].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
-};
+// const nextSlide = () => {
+//     // Get current class
+//     const current = document.querySelector('.current');
+//     // Remove current class
+//     current.classList.remove('current');
+//     // Check for next slide
+//     if (current.nextElementSibling) {
+//         // Add current to next sibling
+//         current.nextElementSibling.classList.add('current');
+//     } else {
+//         // Add current to start
+//         slides[0].classList.add('current');
+//     }
+//     setTimeout(() => current.classList.remove('current'));
+// };
 
-const prevSlide = () => {
-    // Get current class
-    const current = document.querySelector('.current');
-    // Remove current class
-    current.classList.remove('current');
-    // Check for prev slide
-    if (current.previousElementSibling) {
-        // Add current to prev sibling
-        current.previousElementSibling.classList.add('current');
-    } else {
-        // Add current to last
-        slides[slides.length - 1].classList.add('current');
-    }
-    setTimeout(() => current.classList.remove('current'));
-};
-// Button events
-next.addEventListener('click', e => {
-    nextSlide();
-    if (auto) {
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
-});
+// const prevSlide = () => {
+//     // Get current class
+//     const current = document.querySelector('.current');
+//     // Remove current class
+//     current.classList.remove('current');
+//     // Check for prev slide
+//     if (current.previousElementSibling) {
+//         // Add current to prev sibling
+//         current.previousElementSibling.classList.add('current');
+//     } else {
+//         // Add current to last
+//         slides[slides.length - 1].classList.add('current');
+//     }
+//     setTimeout(() => current.classList.remove('current'));
+// };
+// // Button events
+// next.addEventListener('click', e => {
+//     nextSlide();
+//     if (auto) {
+//         clearInterval(slideInterval);
+//         slideInterval = setInterval(nextSlide, intervalTime);
+//     }
+// });
 
-prev.addEventListener('click', e => {
-    prevSlide();
-    if (auto) {
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
-});
-slidedots.addEventListener('click', e => {
-    prevSlide();
-    if (auto) {
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
-});
+// prev.addEventListener('click', e => {
+//     prevSlide();
+//     if (auto) {
+//         clearInterval(slideInterval);
+//         slideInterval = setInterval(nextSlide, intervalTime);
+//     }
+// });
+// slidedots.addEventListener('click', e => {
+//     prevSlide();
+//     if (auto) {
+//         clearInterval(slideInterval);
+//         slideInterval = setInterval(nextSlide, intervalTime);
+//     }
+// });
 
-// Auto slide
-if (auto) {
-    // Run next slide at interval time
-    slideInterval = setInterval(nextSlide, intervalTime);
-}
+// // Auto slide
+// if (auto) {
+//     // Run next slide at interval time
+//     slideInterval = setInterval(nextSlide, intervalTime);
+// }
---
+++ readme.md
@@ -0,0 +1,63 @@
+# JADOO-Travel
+this is my second project at Grazac Academy
+## Table of contents
+
+
+- [JADOO-Travel landing page](#JADOO-Travel landing-page)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+  - [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+# JADOO-Travel landing page
+# JADOO-Contact Us page
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size for the landing page and the contact-us page
+
+
+### Links
+
+Solution Url: (https://github.com/Eyiowuawi/JADOO-Travel)
+
+Vercel Url: (https://jadoo-travel.vercel.app)
+
+## Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Javascript
+
+### What I learned
+
+I learnt the way SVG can be used considering the understanding of viewBox and viewPort. Also It was fascinating to get started with javascript properties in the project, and i hope to fully utilize it in my project and to be best at it  . 
+
+### Challenges
+I encountered challenges with the use of SVG and also with the implementation of stacked carousel using vanilla JavaScript.
+
+### Continued development
+
+I want to focus more on js, media query, and css animations.
+
+### Useful resourses
+
+- [Google Fonts](https://www.fonts.google.com) - This help me with the various fonts used in my project.
+- [Image color picker](https://www.imagecolorpicker.com) - This ihelp me in picking colors from the design given to me.
+- [youtube tutorials](https://www.youtube.com ) -the tutorials on YouTube by Kelvin Powell and many others were a great resource for me.
+- [w3schools](http://w3schools.com) - this was a major resource to get used to css, JavaScript and html
+- [Stack overflow](http://stackoverflow.com) - this was a go back place to resolve some of the bugs I had using git and GitHub.
+
+## Author
+
+- Twitter - [@tolu_wa_laseh](https://www.twitter.com/tolu_wa_laseh)
+- Github - [@eyiowuawi](https://www.github.com/eyiowuawi)
+- LinkedIn- [@adeosuntat] (www.linkedin.com/in/adeosuntat)
+
+## Acknowledgments
+ I thank Mr Mayowa and Kevin Powell my YouTube teacher. Thanks to Grazac for the experience of the challenge.
